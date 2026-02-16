25c8164e76aad5a31890cbd4f34b127ef63c212c
Abdelrahman <abdelrahmannali14@gmail.com>
2026-02-16 14:14:15 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
+
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
-      import('./home.component').then(m => m.HomeComponent)
+      import('./components/home/home.component').then(m => m.HomeComponent)
   }
 ];
