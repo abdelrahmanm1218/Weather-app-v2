24173750b4dde547f8fc1402487440f97e7dbc7a
Abdelrahman <abdelrahmannali14@gmail.com>
2026-02-16 13:40:30 +0200

    Create home.component.ts

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-home',
+  template: `
+    <h2>Home Page 🚀</h2>
+    <p>Project running successfully.</p>
+  `
+})
+export class HomeComponent {}
