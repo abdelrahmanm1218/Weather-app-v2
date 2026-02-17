d5222c2fd060e758c70c0ce319a2a2ac4b6111d4
Abdelrahman <abdelrahmannali14@gmail.com>
2026-02-17 02:39:36 +0200

    Update homepage.component.ts

--- src/app/features/homepage/homepage.component.ts
+++ src/app/features/homepage/homepage.component.ts
@@ -1,1 +1,13 @@
-let t = 1
+import { Component } from '@angular/core';
+
+
+@Component({
+  standalone: true,
+  selector: 'app-homepage',
+  imports: [],
+  template: `
+    <nav style="padding: 10px; background: #eee;">
+    </nav>
+  `
+})
+export class HomepageComponent {}
